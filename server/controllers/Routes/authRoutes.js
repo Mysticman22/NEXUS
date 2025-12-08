1922748e9502146e3aa1170af676c582d7eb1481
Mysticman22 <mysticeldrago@gmail.com>
2025-12-08 23:23:19 +0530

    no need

--- server/controllers/Routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { requestSignupOtp, verifyAndCreateUser } = require('../controllers/authController');
-
-router.post('/request-otp', requestSignupOtp);
-router.post('/verify-otp', verifyAndCreateUser);
-
-module.exports = router;
